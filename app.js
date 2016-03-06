45af363f9a1d74056a61ac0e8f637d70ef54337b
Fernanda Rodriguez Hernandez <fernandardzh@gmail.com>
2016-03-05 22:07:12 -0600

    first version

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict'
+
+/*!
+ * Modules and dependencies
+ */
+
+const Path = require('path');
+const Express = require('express');
+const Morgan = require('morgan');
+
+const app = Express();
+
+/*!
+ * Local vars
+ */
+
+const publicPath = Path.join(__dirname, 'app/public/');
+
+/*!
+ * Using middlewares
+ */
+
+app.use(Morgan('tiny'));
+app.use(Express.static(publicPath));
+
+
+/*!
+ * Routing
+ */
+
+app.get('/', (req,res) => {
+	var indexPath = Path.join(__dirname, 'app/public/index.html');
+	
+	res.sendFile(indexPath);
+});
+
+app.post('/process-vehicle', (req,res) => {
+	res.send('hola camion procesado');
+});
+
+/*!
+ * app up and running
+ */
+
+app.listen(3000, () => {
+	console.log("servidor corriendo en el puerto 3000");
+});
